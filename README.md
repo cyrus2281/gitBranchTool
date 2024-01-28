f56e91a76ec6e02c2989e845608855359c1c8a02
Cyrus Mobini <miladmobini2@gmail.com>
2024-01-28 12:06:05 -0500

    Added G tool - Functions wrapper + autocompletion + new commands

--- README.md
+++ README.md
@@ -1,30 +1,87 @@
 # gitBranchTool
 
+**Tested and supported on: Bash, ZSH, and Git Bash**
+
 > A bash tool to facilitate managing git branch with long cryptic names with aliases
 
-This bash script adds additional functionality to how you use Git. If you frequently work with long branch names that include issue numbers, this tool can be very helpful. With this tool, you'll be able to assign alias names for each branch you create, and switch or delete them based on the alias, instead of the long and confusing name.
+The `gitBranchTool.sh` bash script adds `g` command to your terminal. This command provides additional functionalities around working with *git* branches. 
 
-You can also add notes to each branch to fully remember what they were about, and list all branch names.
+If you frequently work with long branch names that include developer names, project names, issue numbers, and etc this tool is for you. With `gitBranchTool` or `g`, you'll be able to assign **alias names** for **each branch**.
 
-Additionally, the code provides a custom prompt that displays the name of the current repository, branch name, and its alias.
+You can monitor, switch, and delete your branches **using the aliases** instead of the long and confusing branch names (`g` commands **support both branch names and aliases**, so you wouldn't have to use *git* for any of your branch switching needs).
 
-## Functionalities:
+Additionally, You can add notes to each branch to fully remember what they were about so you can keep the aliases shorter. You can list branches with their aliases and notes at anytime.
 
-*  $DEFAULT_BRANCH                        : Default branch name, usually master or main"
-*  $BRANCH_DELIMITER                      : Delimiter for branch info"
-*  $BRANCH_PATH                           : The path where branch info are stored"
-*  $CUSTOMIZED_GIT_PROMPT                 : To whether customize the prompt or not"
-*  currentBranchPath                      : returns the branch path for the current repository"
-*  editCurrentPath                        : opens current branch path in vim for manual editing"
-*  list                                   : Lists all branches with their id, alias, and notes"
-*  addBranchAlias <id> <alias> [<note>]   : Adds alias and note to a branch that is not stored yet"
-*  updateBranchNote <id|alias> <note>     : adds/updates the notes for a branch base on id/alias"
-*  branch <id> <alias> [<note>]           : creates a branch with id, alias, and note, and checks in"
-*  check <id|alias>                       : checks into a branch base on an id or an alias"
-*  del [...<id|alias>]                    : delete listed branches base on id or alias"
-*  currentBranch                          : Returns the name of current branch with alias and note"
-*  branchNameFromAlias <alias>            : Returns the branch name from an alias"
-*  branchHelp                             : shows this list"
+`g` also provides **auto-completion** for branch names and aliases, so you wouldn't even have to type the full alias name.
+
+On top of all these, `g` provides a **custom prompt** that displays the name of the current repository, sub-directory, branch name, and its alias. (You can turn this off if you want to use your own custom prompt).
+
+## Installation
+
+Download the script and run:
+
+```bash
+echo "source PATH_TO_SCRIPT/gitBranchTool.sh" >> ~/.bashrc
+```
+- Replace `PATH_TO_SCRIPT` with the path to the script file.
+- Replace `~/.bashrc` with the path to your bash profile file.
+    - `~/.bashrc` is the default bash profile file for most Linux distributions.
+    - `~/.zshrc` is the default zsh profile file for most MacOS distributions.
+
+### Install without custom prompt
+Alternatively, you can install the script without the custom prompt by setting the environment variable `CUSTOMIZED_GIT_PROMPT` to `false` in your bash profile file before sourcing the script.
+
+```bash
+echo "export CUSTOMIZED_GIT_PROMPT=false" >> ~/.bashrc
+echo "source PATH_TO_SCRIPT/gitBranchTool.sh" >> ~/.bashrc
+```
+- Replace `PATH_TO_SCRIPT` with the path to the script file.
+- Replace `~/.bashrc` with the path to your bash profile file.
 
 
-to add the functionalities, added `source gitBranchAlias.sh` in bash profile.
+
+## Usage:
+```bash
+The following commands can be used with gitBranchTool.
+
+   g <command> [...<args>]
+
+
+*  create <id> <alias> [<note>]           Creates a branch with id, alias, and note, and checks into it
+   c      <id> <alias> [<note>]                  Uses the git command "git checkout -b <id>"
+
+*  check  <id|alias>                      Checks into a branch base on an id or an alias
+   switch <id|alias>                             Uses the git command "git checkout <id>"
+   s      <id|alias>
+
+*  del [...<id|alias>]                    Deletes listed branches base on ID or alias (requires at least one ID/alias)
+   d   [...<id|alias>]                           Uses the git command "git branch -D [...<id>] "
+
+*  list                                   Lists all branches with their id, alias, and notes
+   l
+
+*  resolve-alias <alias>                  Resolves the branch name from an alias
+   r             <alias>
+
+*  add-alias <id> <alias> [<note>]        Adds alias and note to a branch that is not stored yet
+   a         <id> <alias> [<note>]
+
+*  update-branch-alias <id> <alias>       Updates the alias for the given branch id
+
+*  update-branch-note <id|alias> <note>           Adds/updates the notes for a branch base on id/alias
+
+*  current-branch                         Returns the name of active branch with alias and note
+
+*  edit-repository-config                         Opens active repository config file in vim for manual editing
+
+*  help                                   Shows this help menu
+   h
+
+You can set the following parameters in your terminal profile:
+  * DEFAULT_BRANCH                        Default branch name, usually master or main
+  * CUSTOMIZED_GIT_PROMPT                 To whether customize the prompt or not
+  * BRANCH_DELIMITER                      Delimiter for branch info (default '|')
+                                            This character should not be in your branch or alias names
+```
+
+Copyright 2024 Cyrus Mobini (@cyrus2281)
--- gitBranchTool.sh
+++ gitBranchTool.sh
@@ -1,19 +1,63 @@
-__DEFAULT_BRANCH_PATH=~/.branchAliases/.branchAliases
+# Author: Cyrus Mobini - https://github.com/cyrus2281
+# Github Repository: https://github.com/cyrus2281/gitBranchTool
+# License: MIT License
+
+G_VERSION="2.0.1"
+
+__g_help(){
+  echo -e "\nThe following commands can be used with gitBranchTool.\n"
+  echo -e "   g <command> [...<args>]\n"
+  echo -e "\n*  create <id> <alias> [<note>] \t  Creates a branch with id, alias, and note, and checks into it"
+  echo -e "   c      <id> <alias> [<note>]    \t\t Uses the git command \"git checkout -b <id>\""
+  echo -e "\n*  check  <id|alias> \t\t\t  Checks into a branch base on an id or an alias"
+  echo -e "   switch <id|alias> \t\t\t\t Uses the git command \"git checkout <id>\""
+  echo -e "   s      <id|alias>"
+  echo -e "\n*  del [...<id|alias>] \t\t\t  Deletes listed branches base on ID or alias (requires at least one ID/alias)"
+  echo -e "   d   [...<id|alias>] \t\t\t\t Uses the git command \"git branch -D [...<id>] \""
+  echo -e "\n*  list \t\t\t\t  Lists all branches with their id, alias, and notes"
+  echo -e "   l"
+  echo -e "\n*  resolve-alias <alias> \t\t  Resolves the branch name from an alias"
+  echo -e "   r             <alias>"
+  echo -e "\n*  add-alias <id> <alias> [<note>] \t  Adds alias and note to a branch that is not stored yet"
+  echo -e "   a         <id> <alias> [<note>]"
+  echo -e "\n*  update-branch-alias <id> <alias> \t  Updates the alias for the given branch id"
+  echo -e "\n*  update-branch-note <id|alias> <note> \t  Adds/updates the notes for a branch base on id/alias"
+  echo -e "\n*  current-branch \t\t\t  Returns the name of active branch with alias and note"
+  echo -e "\n*  edit-repository-config \t\t\t  Opens active repository config file in vim for manual editing"
+  echo -e "\n*  help \t\t\t\t  Shows this help menu"
+  echo -e "   h"
+  echo -e ""
+  echo -e "You can set the following parameters in your terminal profile:"
+  echo -e "  * DEFAULT_BRANCH \t\t\t  Default branch name, usually master or main"
+  echo -e "  * CUSTOMIZED_GIT_PROMPT \t\t  To whether customize the prompt or not"
+  echo -e "  * BRANCH_DELIMITER \t\t\t  Delimiter for branch info (default '|')"
+  echo -e "                      \t\t\t    This character should not be in your branch or alias names"
+  echo -e "\nGitBranchTool Version: $G_VERSION"
+  echo -e "Created by Cyrus Mobini"
+}
+
+__DEFAULT_BRANCH_PATH=~/.gitBranchTool/.g
 
 BRANCH_DELIMITER=${BRANCH_DELIMITER:-'|'}
 BRANCH_PATH=${BRANCH_PATH:-"$__DEFAULT_BRANCH_PATH"}
 CUSTOMIZED_GIT_PROMPT=${CUSTOMIZED_GIT_PROMPT:-true}
-DEFAULT_BRANCH=main
+DEFAULT_BRANCH=${DEFAULT_BRANCH:-"master"}
+
+if [ -n "$ZSH_VERSION" ]; then
+  # Current shell is ZSH
+  autoload -U +X bashcompinit && bashcompinit
+  autoload -U +X compinit && compinit
+fi
 
 # Git Branch Snippets
-mkdir -p ~/.branchAliases/
+mkdir -p ~/.gitBranchTool/
 
-__getBranchName() {
+__g_get_branch_name() {
   echo $(git branch 2> /dev/null | grep \* | cut -d "*" -f2 | cut -d " " -f2)
 }
 
-currentBranchPath(){
-  if [[ -z $(__getBranchName) ]]; then
+__g_current_branch_path(){
+  if [[ -z $(__g_get_branch_name) ]]; then
     echo "-- Not a git repository --"
     return 1
   fi
@@ -24,36 +68,36 @@ currentBranchPath(){
   echo $currentPath
 }
 
-list(){
-  if [[ -z $(__getBranchName) ]]; then
+__g_list(){
+  if [[ -z $(__g_get_branch_name) ]]; then
     echo "-- Not a git repository --"
     return 1
   fi
-  cat -n $(currentBranchPath) | tr '|' '\t'
+  cat -n $(__g_current_branch_path) | tr '|' '\t'
 }
 
-currentBranch() {
-  if [[ -z $(__getBranchName) ]]; then
+__g_current_branch() {
+  if [[ -z $(__g_get_branch_name) ]]; then
     echo "-- Not a git repository --"
     return 1
   fi
-  cat $(currentBranchPath) | tr '|' '\t' | grep $(__getBranchName)
+  cat $(__g_current_branch_path) | tr '|' '\t' | grep $(__g_get_branch_name)
   if [ $?  != 0 ]; then
       echo "master (or unregistered branch)"
   fi
 }
 
-branchNameFromAlias(){
-  if [[ -z $(__getBranchName) ]]; then
+__g_resolve_alias(){
+  if [[ -z $(__g_get_branch_name) ]]; then
     echo "-- Not a git repository --"
     return 1
   fi
   if (( $# < 1 )); then
     echo "Wrong Usage"
-    echo "\tbranchNameFromAlias <alias>"
+    echo "\t__g_resolve_alias <alias>"
     return 1
   fi
-  cat "$(currentBranchPath)" | while read line || [ -n "$line" ]; do
+  cat "$(__g_current_branch_path)" | while read line || [ -n "$line" ]; do
     id=$(echo "$line" | cut -d "$BRANCH_DELIMITER" -f 1)
     als=$(echo "$line" | cut -d "$BRANCH_DELIMITER" -f 2)
     if [[ $1 == $als ]]; then
@@ -65,22 +109,22 @@ branchNameFromAlias(){
   return 1
 }
 
-editCurrentPath() {
-  vim $(currentBranchPath)
+__g_edit_repo_config() {
+  vim $(__g_current_branch_path)
 }
 
-addBranchAlias(){
-  if [[ -z $(__getBranchName) ]]; then
+__g_add_alias(){
+  if [[ -z $(__g_get_branch_name) ]]; then
     echo "-- Not a git repository --"
     return 1
   fi
   if (( $# < 2 )); then
     echo "Wrong Usage"
-    echo "\taddBranchAlias <id> <alias> [<note>]"
+    echo "\t__g_add_alias <id> <alias> [<note>]"
     return 1
   fi
   # checking for alias to be unique
-  cat "$(currentBranchPath)" | while read line || [ -n "$line" ]; do
+  cat "$(__g_current_branch_path)" | while read line || [ -n "$line" ]; do
     als=$(echo "$line" | cut -d "$BRANCH_DELIMITER" -f 2)
     if [[ $2 == $als ]]; then
         echo '-- Alias should be unique --'
@@ -88,24 +132,31 @@ addBranchAlias(){
         return 1
     fi
   done
-  # Adding branch, alias and note to list only if operation was successful
-  echo "$1$BRANCH_DELIMITER$2$BRANCH_DELIMITER$3" >> "$(currentBranchPath)"
-  echo "-- Added alias '$2' for branch '$1' --"
+  id=$1
+  alias=$2
+  shift 2
+  note=$@
+  # Adding branch, alias and note to list
+  echo "$id$BRANCH_DELIMITER$alias$BRANCH_DELIMITER$note" >> "$(__g_current_branch_path)"
+  echo "-- Added alias '$alias' for branch '$id' --"
 }
 
-updateBranchNote() {
-  if [[ -z $(__getBranchName) ]]; then
+__g_update_branch_note() {
+  if [[ -z $(__g_get_branch_name) ]]; then
     echo "-- Not a git repository --"
     return 1
   fi
   if (( $# < 2 )); then
     echo "Wrong Usage"
-    echo "\tupdateBranchNote <id|alias> <note>"
+    echo "\t__g_update_branch_note <id|alias> <note>"
     return 1
   fi
   found=false
   first=true
-  branchPath=$(currentBranchPath)
+  branchPath=$(__g_current_branch_path)
+  searchParam=$1
+  shift
+  newNote="$@"
   cat "$branchPath" | while read line || [ -n "$line" ]; do
     if [[ $first == true ]]; then
         echo -n "" > "$branchPath"
@@ -115,9 +166,45 @@ updateBranchNote() {
     als=$(echo "$line" | cut -d "$BRANCH_DELIMITER" -f 2)
     note=$(echo "$line" | cut -d "$BRANCH_DELIMITER" -f 3)
     # update note
-    if [[ $1 == $id ||  $1 == $als ]]; then
-      echo "$id$BRANCH_DELIMITER$als$BRANCH_DELIMITER$2" >> "$branchPath"
-      echo "-- update note for '$id  $als' to '$2' --"
+    if [[ $searchParam == $id ||  $searchParam == $als ]]; then
+      echo "$id$BRANCH_DELIMITER$als$BRANCH_DELIMITER$newNote" >> "$branchPath"
+      echo "-- update note for '$id  $als' to '$newNote' --"
+      found=true
+    else
+      echo "$id$BRANCH_DELIMITER$als$BRANCH_DELIMITER$note" >> "$branchPath"
+    fi
+  done
+  if [[ $found == false ]]; then
+      echo "-- branch not found: $searchParam --"
+  fi
+}
+
+__g_update_branch_alias() {
+  if [[ -z $(__g_get_branch_name) ]]; then
+    echo "-- Not a git repository --"
+    return 1
+  fi
+  echo "$@ $#"
+  if (( $# < 2 )); then
+    echo "Wrong Usage"
+    echo "\tg update-branch-alias <id> <note>"
+    return 1
+  fi
+  found=false
+  first=true
+  branchPath=$(__g_current_branch_path)
+  cat "$branchPath" | while read line || [ -n "$line" ]; do
+    if [[ $first == true ]]; then
+        echo -n "" > "$branchPath"
+        first=false
+    fi
+    id=$(echo "$line" | cut -d "$BRANCH_DELIMITER" -f 1)
+    als=$(echo "$line" | cut -d "$BRANCH_DELIMITER" -f 2)
+    note=$(echo "$line" | cut -d "$BRANCH_DELIMITER" -f 3)
+    # update note
+    if [[ $1 == $id ]]; then
+      echo "$id$BRANCH_DELIMITER$2$BRANCH_DELIMITER$note" >> "$branchPath"
+      echo "-- update alias for '$id' from  '$als' to '$2' --"
       found=true
     else 
       echo "$id$BRANCH_DELIMITER$als$BRANCH_DELIMITER$note" >> "$branchPath"
@@ -128,8 +215,8 @@ updateBranchNote() {
   fi
 }
 
-branch(){
-  if [[ -z $(__getBranchName) ]]; then
+__g_branch(){
+  if [[ -z $(__g_get_branch_name) ]]; then
     echo "-- Not a git repository --"
     return 1
   fi
@@ -139,7 +226,7 @@ branch(){
     return 1
   fi
   # checking for alias to be unique
-  cat "$(currentBranchPath)" | while read line || [ -n "$line" ]; do
+  cat "$(__g_current_branch_path)" | while read line || [ -n "$line" ]; do
     als=$(echo "$line" | cut -d "$BRANCH_DELIMITER" -f 2)
     if [[ $2 == $als ]]; then
         echo '-- Alias should be unique --'
@@ -150,8 +237,12 @@ branch(){
   # creating and checking out to branch
   git checkout -b $1
   if [ $? -eq 0 ]; then
+    id=$1
+    alias=$2
+    shift 2
+    note=$@
     # Adding branch, alias and note to list only if operation was successful
-    echo "$1$BRANCH_DELIMITER$2$BRANCH_DELIMITER$3" >> "$(currentBranchPath)"
+    echo "$id$BRANCH_DELIMITER$alias$BRANCH_DELIMITER$note" >> "$(__g_current_branch_path)"
     return 0
   else
     echo '-- FAILED --'
@@ -159,8 +250,8 @@ branch(){
   fi
 }
 
-check(){
-  if [[ -z $(__getBranchName) ]]; then
+__g_check(){
+  if [[ -z $(__g_get_branch_name) ]]; then
     echo "-- Not a git repository --"
     return 1
   fi
@@ -173,7 +264,7 @@ check(){
     git checkout $DEFAULT_BRANCH
     return 0
   fi
-  cat "$(currentBranchPath)" | while read line || [ -n "$line" ]; do
+  cat "$(__g_current_branch_path)" | while read line || [ -n "$line" ]; do
     id=$(echo "$line" | cut -d "$BRANCH_DELIMITER" -f 1)
     als=$(echo "$line" | cut -d "$BRANCH_DELIMITER" -f 2)
     if [[ $1 == $als || $1 == $id ]]; then
@@ -182,16 +273,37 @@ check(){
     fi
   done
   git checkout $1
+  # if successful
   if [[ $?  == 0 ]]; then
-      echo "-- branch \"$1\" is not registered with alias --"
-      return 0
+      # If provided alias
+      if [[ -n $2 ]]; then
+        cat "$(__g_current_branch_path)" | while read line || [ -n "$line" ]; do
+          als=$(echo "$line" | cut -d "$BRANCH_DELIMITER" -f 2)
+          if [[ $2 == $als ]]; then
+              echo '-- Alias should be unique --'
+              echo '-- Branch Switch successfull. Failed to add branch alias. --'
+              return 0 
+          fi
+        done
+        id=$1
+        alias=$2
+        shift 2
+        note=$@
+        # Adding branch, alias and note to list
+        echo "$id$BRANCH_DELIMITER$alias$BRANCH_DELIMITER$note" >> "$(__g_current_branch_path)"
+        echo "-- Branch $id has been registered with alias $alias"
+        return 0
+      else
+        echo "-- branch \"$1\" is not registered with alias --"
+        return 0
+      fi
   fi
   echo "-- branch not found: $1 --"
   return 1
 }
 
-del(){
-  if [[ -z $(__getBranchName) ]]; then
+__g_del(){
+  if [[ -z $(__g_get_branch_name) ]]; then
     echo "-- Not a git repository --"
     return 1
   fi
@@ -200,7 +312,7 @@ del(){
     echo "\tdel <alias|id> [...<alias|id>]"
     return 1
   fi
-  branchPath=$(currentBranchPath)
+  branchPath=$(__g_current_branch_path)
   for value in "$@"; do
     found=false
     first=true
@@ -231,33 +343,135 @@ del(){
   done
 }
 
-branchHelp(){
-  echo "*  \$DEFAULT_BRANCH \t\t\t : Default branch name, usually master or main"
-  echo "*  \$BRANCH_DELIMITER \t\t\t : Delimiter for branch info"
-  echo "*  \$BRANCH_PATH \t\t\t : The path where branch info are stored"
-  echo "*  \$CUSTOMIZED_GIT_PROMPT \t\t : To whether customize the prompt or not"
-  echo "*  currentBranchPath \t\t\t : returns the branch path for the current repository"
-  echo "*  editCurrentPath \t\t\t : opens current branch path in vim for manual editing"
-  echo "*  list \t\t\t\t : Lists all branches with their id, alias, and notes"
-  echo "*  addBranchAlias <id> <alias> [<note>]  : Adds alias and note to a branch that is not stored yet"
-  echo "*  updateBranchNote <id|alias> <note> \t : adds/updates the notes for a branch base on id/alias"
-  echo "*  branch <id> <alias> [<note>] \t : creates a branch with id, alias, and note, and checks in"
-  echo "*  check <id|alias> \t\t\t : checks into a branch base on an id or an alias"
-  echo "*  del [...<id|alias>] \t\t\t : delete listed branches base on id or alias"
-  echo "*  currentBranch \t\t\t : Returns the name of current branch with alias and note"
-  echo "*  branchNameFromAlias <alias> \t\t : Returns the branch name from an alias"
-  echo "*  branchHelp \t\t\t\t : shows this list"
+__g_get_ids(){
+  if [[ -z $(__g_get_branch_name) ]]; then
+    return 0
+  fi
+  branchPath=$(__g_current_branch_path)
+  IDs=()
+  # Read each line of the file
+  while IFS='|' read -r id name desc; do
+    # Add values to the array
+    IDs+=("$id")
+  done < $branchPath
+  echo ${IDs[@]}
 }
 
+__g_get_aliases(){
+  if [[ -z $(__g_get_branch_name) ]]; then
+    return 0
+  fi
+  branchPath=$(__g_current_branch_path)
+  names=()
+  # Read each line of the file
+  while IFS='|' read -r id name desc; do
+    # Add values to the array
+    names+=("$name")
+  done < $branchPath
+  echo ${names[@]}
+}
+
+g() {
+  if (( $# < 1 )); then
+    echo "Invalid command!"
+    echo "\tEnter 'g help' to get a list of all command."
+    return 1
+  fi
+  command=$1
+  shift
+  case $command in
+    "create"|"c")
+      __g_branch $@
+      ;;
+    "check"|"switch"|"s")
+      __g_check $@
+      ;;
+    "del"|"d")
+      __g_del $@
+      ;;
+    "resolve-alias"|"r")
+      __g_resolve_alias $@
+      ;;
+    "add-alias"|"a")
+    __g_add_alias $@
+      ;;
+    "update-branch-alias")
+    __g_update_branch_alias $@
+      ;;
+    "update-branch-note")
+    __g_update_branch_note $@
+      ;;
+    "list"|"l")
+      __g_list
+      ;;
+    "current-branch")
+    __g_current_branch
+      ;;
+    "edit-repository-config")
+    __g_edit_repo_config
+      ;;
+    "help"|"h")
+      __g_help
+      ;;
+  esac
+}
+
+__g_complete() {
+  local cur_word
+  local commands
+  COMPREPLY=()
+  cur_word="${COMP_WORDS[COMP_CWORD]}"
+  prev_word="${COMP_WORDS[COMP_CWORD-1]}"
+  command_word=${COMP_WORDS[1]}
+
+  if [ $COMP_CWORD -eq 1 ]; then
+      commands=("check" "list" "del" "help" "create" "switch" \
+       "resolve-alias" "edit-repository-config" "add-alias" \
+       "update-branch-note" "current-branch" "update-branch-alias" )
+  else
+    case "$command_word" in
+      # Aliases & ID on all args
+      "del"|"d")
+        commands=($(__g_get_ids) $(__g_get_aliases))
+        ;;
+      # Aliases & ID first arg
+      "check"|"switch"|"s"|"update-branch-note")
+        if [ $COMP_CWORD -eq 2 ]; then
+            commands=($(__g_get_ids) $(__g_get_aliases))
+        fi
+        ;;
+      # Aliases only first arg
+      "resolve-alias"|"r")
+        if [ $COMP_CWORD -eq 2 ]; then
+            commands=($(__g_get_aliases))
+        fi
+        ;;
+      # IDs only first arg
+      "update-branch-alias")
+        if [ $COMP_CWORD -eq 2 ]; then
+            commands=($(__g_get_ids))
+        fi
+        ;;
+    esac
+  fi
+
+  # If the current word starts with the characters in one of the commands, suggest it
+  COMPREPLY=($(compgen -W "${commands[*]}" -- "$cur_word"))
+  return 0
+}
+
+# Register the autocompletion function for sayWhat command
+complete -F __g_complete g
+
 # Custom prompt
-__getName() {
+__g_get_name() {
   brn=""
-  if [[ -n $(__getBranchName) ]]; then
+  if [[ -n $(__g_get_branch_name) ]]; then
     name=""
-    cat "$(currentBranchPath)" | while read line || [ -n "$line" ]; do
+    cat "$(__g_current_branch_path)" | while read line || [ -n "$line" ]; do
       id=$(echo "$line" | cut -d "$BRANCH_DELIMITER" -f 1)
       als=$(echo "$line" | cut -d "$BRANCH_DELIMITER" -f 2)
-      if [[ $(__getBranchName) == $id ]]; then
+      if [[ $(__g_get_branch_name) == $id ]]; then
           name=" ($als)"
           break
       fi
@@ -268,7 +482,7 @@ __getName() {
     if [ -n "$subpath" ]; then
       subpath=" [$subpath]"
     fi
-    brn="$repo$subpath ⌥ $(__getBranchName)$name "
+    brn="$repo$subpath ⌥ $(__g_get_branch_name)$name "
   else
     brn="$(pwd) "
   fi
@@ -277,7 +491,7 @@ __getName() {
 
 if [[ $CUSTOMIZED_GIT_PROMPT == true ]]; then
   __update_prompt() {
-    PS1="%n ➤ $(__getName)❖ "
+    PS1="%n ➤ $(__g_get_name)❖ "
   }
   PROMPT_COMMAND=__update_prompt
   precmd() { eval "$PROMPT_COMMAND"; }
