eac4aaa56e06c7f7d44b602bf2e81f54cc9fd88f
Cyrus Mobini <cyrus.mobini@gmail.com>
2024-10-05 19:35:12 -0400

    Added resolveAlias command

--- cmd/root.go
+++ cmd/root.go
@@ -4,7 +4,7 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 
@@ -74,7 +74,8 @@ func initConfig() {
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalln("Error reading config file:", err)
 	}
+	// fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 }
