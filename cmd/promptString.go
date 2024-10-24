bdc657ab3dce48d992d3683535e68cc4b4573ed8
Cyrus Mobini <cyrus.mobini@solace.com>
2024-10-24 17:09:14 -0400

    Added promptstring command + updated custom prompt installation

---
+++ cmd/promptString.go
@@ -0,0 +1,77 @@
+/*
+Copyright © 2024 Cyrus Mobini
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"strings"
+
+	"github.com/cyrus2281/gitBranchTool/internal"
+	"github.com/spf13/cobra"
+)
+
+// promptStringCmd represents the promptString command
+var promptStringCmd = &cobra.Command{
+	Use:    "_ps",
+	Short:  "Returns the prompt string - used for the custom prompt",
+	Long:   `Returns the prompt string - used for the custom prompt`,
+	Hidden: true,
+	Run: func(cmd *cobra.Command, args []string) {
+		username := os.Getenv("LOGNAME")
+		currentUser, err := user.Current()
+		if err == nil {
+			username = currentUser.Username
+		}
+		prompt := fmt.Sprintf("%s ➤ %s ❖ ", username, getPrompt())
+		fmt.Print(prompt)
+	},
+}
+
+// Build the custom prompt string
+func getPrompt() string {
+	workingDirectory, err := os.Getwd()
+	if err != nil {
+		return "$"
+	}
+
+	git := internal.Git{}
+	// Repository name
+	repo, err := git.GetRepositoryName()
+	if err != nil || repo == "" {
+		return workingDirectory
+	}
+
+	// Branch Name
+	currentBranch, err := git.GetCurrentBranch()
+	if err != nil || currentBranch == "" {
+		return workingDirectory
+	}
+
+	// Alias Name
+	repoBranches := internal.GetRepositoryBranches()
+	branch, ok := repoBranches.GetBranchByName(currentBranch)
+	if ok && branch.Alias != "" {
+		currentBranch = fmt.Sprintf("%s (%s)", currentBranch, branch.Alias)
+	}
+
+	// Subpath
+	subpath := ""
+	index := strings.Index(workingDirectory, repo)
+	if index >= 0 {
+		subpath = workingDirectory[index+len(repo):]
+		subpath = strings.TrimPrefix(subpath, "/")
+		if subpath != "" {
+			subpath = fmt.Sprintf(" [%s]", subpath)
+		}
+	}
+
+	// Custom Prompt String
+	return fmt.Sprintf("%s%s ⌥ %s", repo, subpath, currentBranch)
+}
+
+func init() {
+	rootCmd.AddCommand(promptStringCmd)
+}
